7a05fe2ba75df2d89c92ca77190f3acf0cc71506
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-11 20:40:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   holiday_hash.each do |season, day|
-    if season == :fall
+    if season == season
       holiday_hash[season] = {"#{holiday_name}": supply_array}
     end
   end
