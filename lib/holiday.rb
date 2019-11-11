d2a95df39e2dfe845198d804ff968284c29fb3af
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-11 20:56:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,7 @@ def all_winter_holiday_supplies(holiday_hash)
       winter_supplies = []
       day.each do |name, data|
           winter_supplies << data
-          binding.pry
+          #binding.pry
       end
     end
   end
